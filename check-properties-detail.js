aaeac06e6d897ab30c8f4a6158fb5b9db2b9c878
Taisuke Fukuno <fukuno@jig.jp>
2023-08-04 08:27:44 +0900

    add err page

--- check-properties-detail.js
+++ check-properties-detail.js
@@ -1,6 +1,10 @@
 import { CSV } from "https://js.sabae.cc/CSV.js";
+import { writeData } from "https://js.sabae.cc/writeData.js";
 
 const props = await CSV.fetchJSON("./japan-heritage_props_detail.csv");
 const sum = props.length;
-const err = props.filter(p => !p.lat).length;
+const errprops = props.filter(p => !p.lat);
+const err = errprops.length;
 console.log(err, sum, (err / sum * 100).toFixed(1));
+
+await writeData("japan-heritage_props_detail_err", errprops);
